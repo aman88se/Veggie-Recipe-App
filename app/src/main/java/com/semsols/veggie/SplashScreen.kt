cceb7aef093f6a97ce002ef369a8269950e62b0d
Aman Semwal <amansemwal542@gmail.com>
2022-02-01 13:13:37 +0900

    Recipe App

--- app/src/main/java/com/semsols/veggie/SplashScreen.kt
+++ app/src/main/java/com/semsols/veggie/SplashScreen.kt
@@ -1,11 +1,20 @@
 package com.semsols.veggie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        startBtn.setOnClickListener{
+
+            startActivity(Intent(this@SplashScreen,HomeScreen::class.java))
+            finish()
+
+        }
     }
 }
